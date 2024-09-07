c50cbb06c9b23cb599c05d64fdda786315efe30d
Vinayaka <vinayakbhat430@gmail.com>
2024-09-07 11:04:22 +0530

    adding test for application

---
+++ README.md
@@ -0,0 +1,3 @@
+### Command to set the secret (Object creation without yaml)
+
+`kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf`
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,44 +1,5 @@
-
-import express from 'express';
-import 'express-async-errors';
 import mongoose from 'mongoose';
-
-
-import { CurrentUserRouter } from "./routes/current-user";
-import { SignInRouter } from "./routes/signin";
-import { SignOutRouter } from "./routes/signout";
-import { SignUpRouter } from "./routes/signup";
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-import cookieSession from 'cookie-session';
-
-
-
-const app = express()
-app.set('trust proxy',true)
-
-app.use(express.json())
-
-app.use(
-    cookieSession({
-        signed:false,
-        secure:true
-    })
-)
-
-
-app.use(SignInRouter)
-app.use(SignOutRouter)
-app.use(SignUpRouter)
-app.use(CurrentUserRouter)
-
-app.all('*',async ()=>{
-    throw new NotFoundError()
-})
-
-
-//error handler should be used in end after all the usage of routes else error handler wont work
-app.use(errorHandler)
+import {app} from './app';
 
 const start = async () => {
     if(!process.env.JWT_KEY){
@@ -51,9 +12,9 @@ const start = async () => {
     catch(err){
         console.log(err)
     }
+    app.listen(3000,()=>{
+        console.log("listening -> auth -> 3000");
+    })
 }
-
-app.listen(3000,()=>{
-    console.log("listening -> auth -> 3000");
-})
 start();
+
