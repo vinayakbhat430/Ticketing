4428c150317a769178ddd4bc3d5b90e71c801daf
Vinayaka <vinayakbhat430@gmail.com>
2024-10-02 20:47:15 +0530

    Version Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -17,4 +17,5 @@ export * from './events/base-publisher';
 export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
+export * from './events/types/types';
 
